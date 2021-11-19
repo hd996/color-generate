836966ee27cf8c156ed936a320aa80edd5c5e18f
侯栋 <houdong111@gmail.com>
2021-11-19 12:28:53 +0800

    feat: update bin color-generate.js

---
+++ bin/util.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateFile = void 0;
+var fs_1 = __importDefault(require("fs"));
+var generateFile = function (colors, template) {
+    try {
+        var data_1 = fs_1.default.readFileSync("templates/" + template + ".tpl", 'utf8');
+        colors.map(function (color, index) {
+            data_1 = data_1.replace(new RegExp("\\$\\{colorPrimary" + index + "\\}", 'mg'), color);
+        });
+        fs_1.default.writeFileSync(process.cwd() + "/" + template, data_1);
+    }
+    catch (e) {
+        console.error('generate file error: ', e);
+    }
+};
+exports.generateFile = generateFile;
